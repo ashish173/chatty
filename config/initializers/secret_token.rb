1b73230e242e72ae232e45db26b739759aad0fe3
Ashish Singh <aitashish173@gmail.com>
2014-10-29 13:02:30 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cleanfb::Application.config.secret_key_base = 'da4c594fa81af9f718e42c63b13e4a79998635884b80f37c7463d39667387b9897a0e9e6505fc59b05792dad47eb9a83d08d51c146996e7da60b8269ed36ab0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cleanfb::Application.config.session_store :cookie_store, key: '_cleanfb_session'
