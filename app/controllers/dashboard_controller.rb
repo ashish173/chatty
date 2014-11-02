c7b1c0d40c4e5847ade15ca7c31a32308ad3ae40
Ashish Singh <aitashish173@gmail.com>
2014-11-02 17:22:50 +0530

    Devise integrated with user model

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,5 @@
+class DashboardController < ApplicationController
+  def index
+    puts "===>>>>>> In dashboard controller"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Cleanfb::Application.routes.draw do
+  devise_for :users
+  
+  resources :dashboard
+  root 'dashboard#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
